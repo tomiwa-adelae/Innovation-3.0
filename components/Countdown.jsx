179b956c7e07aa03ea1d5372739a2f8b68f47c32
Tomiwa Adelae <thetommedia@gmail.com>
2024-04-14 13:15:57 +0100

    Updated the event date for the countdown

--- components/Countdown.jsx
+++ components/Countdown.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const Countdown = () => {
-	const launchDate = new Date("May 10, 2024, 09:00:00").getTime();
+	const launchDate = new Date("May 17, 2024, 09:00:00").getTime();
 
 	const [days, setDays] = useState(0);
 	const [hours, setHours] = useState(0);
