487598b6c0fdc64f08b8b587c200d3364c7c02f0
Tomiwa Adelae <thetommedia@gmail.com>
2024-05-02 22:28:54 +0100

    Added a testimony

--- app/data/testimonials.jsx
+++ app/data/testimonials.jsx
@@ -1,5 +1,12 @@
 export const testimonials = [
 	{
+		imagePath: "/jegede-surprise.jpg",
+		name: "Surprise Jegede",
+		testimony:
+			"What a way to celebrate my birthday! I attended Innovation 2.0, and I'm still reeling from the experience. This gathering was truly a game-changer, and I feel so grateful to have been a part of it. From the moment I walked in, I knew I was in for something special. The energy in the room was electric, and the excitement was palpable. The speakers were absolutely phenomenal, sharing cutting-edge ideas and insights that left me in awe. I was fascinated by the innovative solutions presented, and I found myself scribbling notes furiously, eager to absorb every bit of knowledge. What struck me most was the diversity of perspectives and expertise. The event brought together individuals from various fields, all united by a passion for innovation and progress. I had the opportunity to engage with like-minded individuals, exchange ideas, and form meaningful connections that I know will last a lifetime. The event was expertly curated, with a perfect blend of keynote speeches, discussions, and interactive sessions. I was particularly impressed by the emphasis on practical applications and real-world examples, making it feel accessible and achievable. Innovation 2.0 was more than just an event – it was an experience that ignited a spark within me. I left feeling inspired, motivated, and empowered to make a difference in my own sphere. It was a powerful reminder that we all have the capacity to create positive change and shape the future. As I reflect on my birthday celebration, I realize that this event was the perfect gift – a chance to learn, grow, and connect with others who share my passion for innovation. I'm already looking forward to the next iteration of Innovation 2.0, and I encourage anyone who's interested in shaping the future to join me there. Thank you to the organizers, volunteers, speakers, and fellow attendees for making it an unforgettable experience!",
+		portfolio: "Student of ACU",
+	},
+	{
 		imagePath: "/emmanuella.jpg",
 		name: "Emmanuella",
 		testimony:
@@ -7,18 +14,10 @@ export const testimonials = [
 		portfolio: "Student of ACU",
 	},
 	{
-		imagePath: "/jegede-surprise.jpg",
-		name: "Surprise Jegede",
-		testimony:
-			"Innovation 2.0 was a blast. That is one word to describe the experience with. I was really amazed",
-		portfolio: "Student of ACU",
-	},
-
-	{
 		imagePath: "/user-icon.jpg",
 		name: "Dapo",
 		testimony: "Innovation 2.0 was fire. I was very very inspired",
-		portfolio: "Student of Ajayi Crowther University",
+		portfolio: "Student of ACU",
 	},
 
 	{
